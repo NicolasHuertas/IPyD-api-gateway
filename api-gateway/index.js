d637217cc15e37fbaaac27d34cf00b05735f6ffb
NicolasHuertas <98675931+NicolasHuertas@users.noreply.github.com>
2024-07-08 21:16:13 -0500

    Adapted for IPyD

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -4,60 +4,21 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port =  8005;
 
-app.use('/login', createProxyMiddleware({ 
-  target: 'https://authentication-production-f0de.up.railway.app', 
-  changeOrigin: true
-}));
-
-app.use('/quotation', createProxyMiddleware({ 
-  target: 'http://bemanager-quotes:8000', 
-  changeOrigin: true,
-  pathRewrite: {
-    '^/quotation': 'quotation/'
-  }
-}));
-
-app.use('/clients', createProxyMiddleware({ 
-  target: 'http://bemanager-clients:8989', 
-  changeOrigin: true,
-  pathRewrite: {
-    '^/clients': 'listado/'
-  }
-}));
-
-app.use('/providers', createProxyMiddleware({ 
-    target: 'http://bemanager-providers:8420', 
+app.use('/reservations', createProxyMiddleware({ 
+    target: 'http://combined-reservations:8420', 
     changeOrigin: true,
     pathRewrite: {
-      '^/providers': 'providers/api/providers/'
+      '^/reservations': 'reservations/'
     }
 }));
 
-app.use('/articles', createProxyMiddleware({ 
-  target: 'http://bemanager-providers:8420', 
+app.use('/hotels', createProxyMiddleware({ 
+  target: 'http://hotel-reservations:3000', 
   changeOrigin: true,
   pathRewrite: {
-    '^/articles': 'providers/api/articles/'
+    '^/hotels': 'reservations/api/reservations'
   }
 }));
-
-app.use('/cashflow', createProxyMiddleware({ 
-  target: 'http://bemanager-cash-flow:8787', 
-  changeOrigin: true,
-  pathRewrite: {
-    '^/cashflow': 'app/'
-  }
-}));
-
-app.use('/category', createProxyMiddleware({ 
-  target: 'http://bemanager-products:8686', 
-  changeOrigin: true,
-  pathRewrite: {
-    '^/category': 'category/'
-  }
-}));
-
-
 
 app.listen(port, () => {
    console.log(`Api Gateway service listening at http://127.0.0.1:${port}`)
